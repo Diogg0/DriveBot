1700743b89f41b7ceed604138acee9254e3a2078
Diogg0 <115207736+Diogg0@users.noreply.github.com>
2022-10-18 00:29:37 +0530

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ import auth
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/drive '
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'client_secret.json' # <-- name of the client meta_data (Refer to read_me.txt)
 APPLICATION_NAME = 'Drive API Python Quickstart'
 authInst = auth.auth(SCOPES, CLIENT_SECRET_FILE, APPLICATION_NAME)
 credentials = authInst.getCredentials()
@@ -16,5 +16,5 @@ drive_service = discovery.build('drive', 'v3', http=http)
 
 
 # Discord Requirements
-TOKEN = 'MTAzMDg2ODA4NDA1Mjk5MjAxMA.G5xOHC.0XcUFHMx48EBayvLpaYtiuQRyNSixUD3dFYH8E'
-GUILD_ID = 854057215543214150
+TOKEN = '<Token goes here>'
+GUILD_ID = 1234567364746 # <-- Guild ID goes here
